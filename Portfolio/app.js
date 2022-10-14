2d0ff0e3ba1bf984bbcf42ce90132170338f26f4
dinhbaokhanh <khanhdbao@gmail.com>
2022-10-14 15:29:44 +0700

    commit

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,5 +1,63 @@
+// skill bars
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            entry.target.classList.add('skill-pers');
+            return
+        }
+        
+    });
+});
+for (let i = 0; i < document.querySelectorAll('.skill-per').length; i++) {
+    observer.observe(document.querySelectorAll('.skill-per')[i]);;
+}
+
+
+const animations_in_education = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            entry.target.classList.add('w3-animate-zoom');
+            return
+        }
+    });
+});
+for (let i = 0; i < document.querySelectorAll('.box').length; i++) {
+    animations_in_education.observe(document.querySelectorAll('.box')[i]);;
+}
+  
+const typing = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            entry.target.classList.add('typing');
+            return
+        }
+    });
+});
+for (let i = 0; i < document.querySelectorAll('.no-typing').length; i++) {
+    typing.observe(document.querySelectorAll('.no-typing')[i]);
+}
+
+
+// observer.observe(document.querySelectorAll('.skill-per'));
+console.log(document.querySelectorAll('.no-typing').length)
+
+
+
+
+
+// scroll to top
 let mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -10,20 +68,17 @@ function scrollFunction() {
     }
 }
 
+// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.documentElement.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
+// responsive
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
